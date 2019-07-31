50cc8eb671128531aee00fb625a942659e6c1695
Johann Montfort <jmontfort@deloitte.com>
2019-07-31 09:56:59 +0200

    Added the Redux inital

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import Posts from './components/Posts';
 import Postform from './components/Postform';
 
+import store from './store';
+
 function App() {
   return (
-    <div className="App">
-      <Postform />
-      <hr/>
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Postform />
+        <hr/>
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
